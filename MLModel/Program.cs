1ef6b454933c757c4c348d05ba0a9fb5250991a3
Apostu Daniel <apostudanbs@gmail.com>
2022-04-03 15:42:51 +0300

    Splited data into training and test data

--- MLModel/Program.cs
+++ MLModel/Program.cs
@@ -4,18 +4,23 @@
     {
         static void Main(string[] args)
         {
-            SplitData();
+            //SplitData();
+            //Console.WriteLine("Done splitting");
+
         }
 
         public static void SplitData()
         {
-            string[] dataset = File.ReadAllLines(@".\Data\rating.csv");
+            string[] dataset = File.ReadAllLines(@"../../../Data\rating.csv");
   
             int numLines = dataset.Length;
             var body = dataset.Skip(1);
-            var sorted = body.Select(line => new { SortKey = Int32.Parse(line.Split(',')[3]), Line = line })
+
+            //sortare dupa data la care au fost facute ratingurile
+            var sorted = body.Select(line => new { SortKey = DateTime.Parse(line.Split(',')[3]).Ticks, Line = line })
                              .OrderBy(x => x.SortKey)
                              .Select(x => x.Line);
+            //90% for fi date pentru training si 10% date pentru testare
             File.WriteAllLines(@"../../../Data\ratings_train.csv", dataset.Take(1).Concat(sorted.Take((int)(numLines * 0.9))));
             File.WriteAllLines(@"../../../Data\ratings_test.csv", dataset.Take(1).Concat(sorted.TakeLast((int)(numLines * 0.1))));
         }

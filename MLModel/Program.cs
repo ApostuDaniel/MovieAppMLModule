dbe77fd3541daf3aa0ce16080fc646671531ff50
Apostu Daniel <apostudanbs@gmail.com>
2022-04-03 14:29:36 +0300

    Added rating data without timestamp column.

--- MLModel/Program.cs
+++ MLModel/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace MLModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SplitData();
+        }
+
+        public static void SplitData()
+        {
+            string[] dataset = File.ReadAllLines(@".\Data\rating.csv");
+  
+            int numLines = dataset.Length;
+            var body = dataset.Skip(1);
+            var sorted = body.Select(line => new { SortKey = Int32.Parse(line.Split(',')[3]), Line = line })
+                             .OrderBy(x => x.SortKey)
+                             .Select(x => x.Line);
+            File.WriteAllLines(@"../../../Data\ratings_train.csv", dataset.Take(1).Concat(sorted.Take((int)(numLines * 0.9))));
+            File.WriteAllLines(@"../../../Data\ratings_test.csv", dataset.Take(1).Concat(sorted.TakeLast((int)(numLines * 0.1))));
+        }
+    }
+}

6217db4d870f51451414722d8e90ac9ffc9e9610
Apostu Daniel <apostudanbs@gmail.com>
2022-04-16 14:59:39 +0300

    Created new controller class

---
+++ MLModelMovies_WebApi/Controllers/RecomandationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MLModelMovies_WebApi.Controllers
+{
+    public class RecomandationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
